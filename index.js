405c8b79bc581f7aa4e2b14013182758194ec91d
Ryan Acham <ryanacham@hotmail.com>
2022-06-14 21:45:58 -0400

    created inital functions as well as blank html template files

---
+++ index.js
@@ -0,0 +1,46 @@
+const manager = require();
+const engie = require();
+const intern = require();
+
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+function start() {
+    managerQuery();
+}
+
+function managerQuery() {
+    inquirer
+        .prompt([
+
+        ])
+}
+
+function addMember() {
+    inquirer
+        .prompt([
+
+        ])
+}
+
+function enigeQuery() {
+    inquirer
+        .prompt([
+
+        ])
+}
+
+
+function internQuery() {
+    inquirer
+        .prompt([
+
+        ])
+}
+
+function createFile() {
+
+}
+
+start();
