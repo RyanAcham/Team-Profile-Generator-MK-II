fbd03f931daeacd37c6e4eadf46636dd8e63c8f5
Ryan Acham <ryanacham@hotmail.com>
2022-06-14 22:33:54 -0400

    filled in class files in lib

---
+++ library/employee.js
@@ -0,0 +1,25 @@
+class employee {
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+      this.title = "employee";
+    }
+  
+    getName() {
+      return this.name;
+    }
+  
+    getId() {
+      return this.id;
+    }
+  
+    getEmail() {
+      return this.email;
+    }
+  
+    getRole() {
+      return this.title;
+    }
+  }
+  module.exports = employee;
